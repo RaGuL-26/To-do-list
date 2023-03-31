423cf1ea9c437cd9c0ff81e666d3300902b44229
RaGuL-26 <91547338+RaGuL-26@users.noreply.github.com>
2023-03-31 12:44:41 +0530

    Add files via upload

---
+++ to do/java.js
@@ -0,0 +1,12 @@
+const inputField = document.getElementById('one');
+const buttonField = document.getElementById('two');
+
+let data = [];
+
+function enter(){
+  let newtodo = document.createElement('li');
+  newtodo.textContent = inputField.value ;
+document.querySelector('body').appendChild(newtodo)}
+
+
+
